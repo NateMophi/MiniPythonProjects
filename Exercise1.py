6f1219eeb7786415edfc2efd4fd1f22f41bd1c9f
NateMophi <66631222+NateMophi@users.noreply.github.com>
2022-12-31 15:03:15 +0100

    Update Exercise1.py

--- Exercise1.py
+++ Exercise1.py
@@ -7,17 +7,17 @@ import re
 # and character classes.
 
 
-# def check_web_address(text):
-#     pattern1 = r"^[\w\.+-]+\.[a-zA-Z]+$"
-#     result = re.search(pattern1, text)
-#     return result != None
+def check_web_address(text):
+    pattern1 = r"^[\w\.+-]+\.[a-zA-Z]+$"
+    result = re.search(pattern1, text)
+    return result != None
 
 
-# print(check_web_address("gmail.com"))  # True
-# print(check_web_address("www@google"))  # False
-# print(check_web_address("www.Coursera.org"))  # True
-# print(check_web_address("web-address.com/homepage"))  # False
-# print(check_web_address("My_Favorite-Blog.US"))  # True
+print(check_web_address("gmail.com"))  # True
+print(check_web_address("www@google"))  # False
+print(check_web_address("www.Coursera.org"))  # True
+print(check_web_address("web-address.com/homepage"))  # False
+print(check_web_address("My_Favorite-Blog.US"))  # True
 
 # Question2
 # The check_time function checks for the time format of a 12-hour clock, as follows: the hour is between 1 and 12, with
@@ -25,17 +25,17 @@ import re
 # Fill in the regular expression to do that. How many of the concepts that you just learned can you use here?
 
 
-# def check_time(text):
-#     pattern2 = r"(?i)^(1[0-2]|0?[1-9]):[0-5][0-9]\s?(AM|PM)$"
-# #r"^(0?[1-9] | 1[0-2]):[0-5][0-9]\s?(AM|PM)$"
-#     result = re.search(pattern2, text)
-#     return result != None
+def check_time(text):
+    pattern2 = r"(?i)^(1[0-2]|0?[1-9]):[0-5][0-9]\s?(AM|PM)$"
+#r"^(0?[1-9] | 1[0-2]):[0-5][0-9]\s?(AM|PM)$"
+    result = re.search(pattern2, text)
+    return result != None
 
 
-# print(check_time("12:45pm"))  # True
-# print(check_time("9:59 AM"))  # True
-# print(check_time("6:60am"))  # False
-# print(check_time("five o'clock"))  # False
+print(check_time("12:45pm"))  # True
+print(check_time("9:59 AM"))  # True
+print(check_time("6:60am"))  # False
+print(check_time("five o'clock"))  # False
 
 
 # Question3
@@ -44,20 +44,20 @@ import re
 # For example, "Instant messaging (IM) is a set of communication technologies used for text-based communication"
 # hould return True since (IM) satisfies the match conditions." Fill in the regular expression in this function:
 
-# def contains_acronym(text):
-#     pattern3 = r"\((0-9)|([A-Z]).+\)"
-#     result = re.search(pattern3, text)
-#     return result != None
+def contains_acronym(text):
+    pattern3 = r"\((0-9)|([A-Z]).+\)"
+    result = re.search(pattern3, text)
+    return result != None
 
 
-# print(contains_acronym(
-#     "Instant messaging (IM) is a set of communication technologies used for text-based communication"))  # True
-# print(contains_acronym("American Standard Code for Information Interchange (ASCII) is a character encoding standard for electronic communication"))  # True
-# print(contains_acronym("Please do NOT enter without permission!"))  # False
-# print(contains_acronym(
-#     "PostScript is a fourth-generation programming language (4GL)"))  # True
-# print(contains_acronym(
-#     "Have fun using a self-contained underwater breathing apparatus (Scuba)!"))  # True
+print(contains_acronym(
+    "Instant messaging (IM) is a set of communication technologies used for text-based communication"))  # True
+print(contains_acronym("American Standard Code for Information Interchange (ASCII) is a character encoding standard for electronic communication"))  # True
+print(contains_acronym("Please do NOT enter without permission!"))  # False
+print(contains_acronym(
+    "PostScript is a fourth-generation programming language (4GL)"))  # True
+print(contains_acronym(
+    "Have fun using a self-contained underwater breathing apparatus (Scuba)!"))  # True
 
 # Question 6
 # Fill in the code to check if the text passed includes a possible U.S. zip code, formatted as follows:
